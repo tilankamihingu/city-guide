5f9c1ac35387dd49601cd23b962400d47b818885
tilankamihingu <tilankamihingu2018@gmail.com>
2020-08-19 01:24:09 -0700

    Updates

---
+++ precache-manifest.07c8b537dfd6e6db17e8bab53cb697fb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e39c86f8b5f2e3fe62eb967885ebe1",
+    "url": "/city-guide/index.html"
+  },
+  {
+    "revision": "1ba1dff89f6012636e9d",
+    "url": "/city-guide/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a20f5f01d2c79ec38ae8",
+    "url": "/city-guide/static/css/main.753e7d30.chunk.css"
+  },
+  {
+    "revision": "1ba1dff89f6012636e9d",
+    "url": "/city-guide/static/js/2.ebb2361b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/city-guide/static/js/2.ebb2361b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a20f5f01d2c79ec38ae8",
+    "url": "/city-guide/static/js/main.17916c3b.chunk.js"
+  },
+  {
+    "revision": "c23233a800d7cb04e7be",
+    "url": "/city-guide/static/js/runtime-main.1cb82775.js"
+  },
+  {
+    "revision": "f391e657f9cabe91f018ca14e340d693",
+    "url": "/city-guide/static/media/logo.f391e657.svg"
+  }
+]);
